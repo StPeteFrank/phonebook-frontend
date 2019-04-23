a737eee164ad78041f7f01555832e4815923d968
Frank Diaz <diazfrankp@gmail.com>
2019-04-23 16:48:20 -0400

    Initial html and css table

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ function App() {
     <div className="App">
       <div className="TitleContainer">
         <h1>Phone Book</h1>
+        <h2>Contacts</h2>
+      </div>
+      <div className="AddEditDeleteContactsTab">
+        <h3>ADD/EDIT/DELETE CONTACTS</h3>
+      </div>
+      <div className="ContactsContainer">
+        <table className="ContactsTable">
+          <tbody>
+            <tr>
+              <th>FIRST</th>
+              <th>LAST</th>
+              <th>PHONE</th>
+              <th>EMAIL</th>
+            </tr>
+            <tr>
+              <td>Frank</td>
+              <td>Diaz</td>
+              <td>999999999999</td>
+              <td>zzz@yyy.com</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
