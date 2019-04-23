2c231c2f7d22232edbf7f52a361b569243101fee
Frank Diaz <diazfrankp@gmail.com>
2019-04-23 16:12:37 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="TitleContainer">
+        <h1>Phone Book</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
