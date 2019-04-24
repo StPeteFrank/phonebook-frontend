2d80cf1424acaa1ed94aa60469583c85e0949bbb
Frank Diaz <diazfrankp@gmail.com>
2019-04-24 14:54:35 -0400

    Started auth0 process

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Contacts from './Contacts'
+import Callback from './Callback'
+import LogIn from './LogIn'
+//import auth0 from './Auth0'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <script
+          type="text/javascript"
+          src="node_modules/auth0-js/build/auth0.js"
+        />
         <div className="App">
-          <div className="TitleContainer">
+          {/* <div className="TitleContainer">
             <h1>Phone Book</h1>
           </div>
           <div className="Tabs">
             <Link to="/contacts">
               <button>CONTACTS</button>
             </Link>
-          </div>
+          </div> */}
 
           <div />
 
           {/* <Route exact path="/" component={Home} /> */}
           <Route path="/contacts" component={Contacts} />
+          <Route path="/logIn" component={LogIn} />
+          <Route path="/callback" component={Callback} />
         </div>
       </Router>
     )
---
+++ src/Auth0.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Auth0 extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export default Auth0
