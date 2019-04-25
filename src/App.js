83a97abbe818e77032f69af0a42b0d769958a8ed
Frank Diaz <diazfrankp@gmail.com>
2019-04-25 13:32:09 -0400

    Added Home.j page. Removed title from App.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './Home'
 import Contacts from './Contacts'
 import Callback from './Callback'
 import LogIn from './LogIn'
@@ -15,18 +16,8 @@ class App extends Component {
           src="node_modules/auth0-js/build/auth0.js"
         />
         <div className="App">
-          {/* <div className="TitleContainer">
-            <h1>Phone Book</h1>
-          </div>
-          <div className="Tabs">
-            <Link to="/contacts">
-              <button>CONTACTS</button>
-            </Link>
-          </div> */}
-
-          <div />
-
-          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
           <Route path="/contacts" component={Contacts} />
           <Route path="/logIn" component={LogIn} />
           <Route path="/callback" component={Callback} />
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="TitleContainer">
+          <h1>Phone Book</h1>
+        </div>
+        <div className="Tabs">
+          <Link to="/contacts">
+            <button>CONTACTS</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
